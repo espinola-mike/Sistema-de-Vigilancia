1714a80662e7768c3002cefa68ebfe904a0fb744
Mike Espinola <espinola.mike14@gmail.com>
2022-11-15 13:59:54 -0400

    Formulario de fechas modificado

--- app/static/js/ondemand.js
+++ app/static/js/ondemand.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', e => {
+function detectar_fecha_actual () {
     let fechaInicial = document.getElementById('fecha_inicial');
     let fechaFinal = document.getElementById('fecha_final');
     let fecha = new Date();
@@ -7,4 +7,19 @@ document.addEventListener('DOMContentLoaded', e => {
     fechaFinal.setAttribute('max', fecha.toLocaleDateString().split('/').reverse().join('-'));
     fechaInicial.setAttribute('value', fecha.toLocaleDateString().split('/').reverse().join('-'));
     fechaFinal.setAttribute('value', fecha.toLocaleDateString().split('/').reverse().join('-'));
+}
+
+
+document.addEventListener('DOMContentLoaded', e => {
+    detectar_fecha_actual();
+    let fechaInicial = document.getElementById('fecha_inicial');
+    let fechaFinal = document.getElementById('fecha_final');
+    fechaFinal.addEventListener('click', e => {
+        fechaFinal.setAttribute('min', fechaInicial.value)
+        fechaInicial.setAttribute('max', fechaFinal.value)
+    })
+    fechaInicial.addEventListener('click', e => {
+        fechaInicial.setAttribute('max', fechaFinal.value)
+        fechaFinal.setAttribute('min', fechaInicial.value)
+    })
 });
